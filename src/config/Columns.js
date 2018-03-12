0a80a1489d6542717def8874d41946472569e0ed
Daniel Capacio <danielcapacio@gmail.com>
2018-03-11 21:00:46 -0700

    Filter by column functionality

--- src/config/Columns.js
+++ src/config/Columns.js
@@ -1,13 +1,36 @@
+import { textFilter, selectFilter } from 'react-bootstrap-table2-filter';
+
+const categoryOptions = {
+    0: 'Utilities',
+    1: 'Groceries',
+    2: 'Household',
+    3: 'Restaurants',
+    4: 'Clothing',
+    5: 'Entertainment',
+    6: 'Hobbies',
+    7: 'Work',
+    8: 'Insurance',
+    9: 'Savings',
+    10: 'Other',
+};
+
 export const columns = [{
     dataField: 'date',
-    text: 'Date'
+    text: 'Date',
+    filter: textFilter()
 }, {
     dataField: 'category',
-    text: 'Category'
+    text: 'Category',
+    formatter: cell => categoryOptions[cell],
+    filter: selectFilter({
+        options: categoryOptions
+    })
 }, {
     dataField: 'description',
-    text: 'Description'
+    text: 'Description',
+    filter: textFilter()
 }, {
     dataField: 'amount',
-    text: 'Amount'
+    text: 'Amount',
+    filter: textFilter()
 }];
--- src/config/TestExpenses.js
+++ src/config/TestExpenses.js
@@ -4,35 +4,35 @@ export const expenses = [
     {
       id: 1,
       date: '2018-03-01',
-      category: 'Restaurants',
+      category: 3,
       description: 'Sushi Town',
       amount: 21.28
     },
     {
       id: 2,
       date: '2018-03-03',
-      category: 'Entertainment',
+      category: 5,
       description: 'Black Panther',
       amount: 16.75
     },
     {
       id: 3,
       date: '2018-03-05',
-      category: 'Clothing',
+      category: 4,
       description: 'Denim jacket',
       amount: 177.86
     },
     {
       id: 4,
       date: '2018-03-08',
-      category: 'Restaurants',
+      category: 3,
       description: 'Cactus Club Cafe',
       amount: 48.77
     },
     {
       id: 5,
       date: '2018-03-11',
-      category: 'Groceries',
+      category: 1,
       description: 'Milk, eggs, celery, ketchup',
       amount: 14.22
     }
