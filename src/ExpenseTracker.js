f852e8ce3a174d754be2fc5146a09a37a0fa4624
Daniel Capacio <danielcapacio@gmail.com>
2018-03-11 20:07:32 -0700

    material-ui dependency

--- src/ExpenseTracker.js
+++ src/ExpenseTracker.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './ExpenseTracker.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 class ExpenseTracker extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-        </p>
-      </div>
+      <MuiThemeProvider>
+        <div className="App">
+          <h1>
+            Expense Tracker
+          </h1>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
